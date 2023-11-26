45a6c772f3f2a1e54c75fcf7e7cc1ec89a0bd6e4
belki <belki@DESKTOP-71T48VK>
2023-11-26 21:10:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Проект2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Add = new System.Windows.Forms.Button();
+            this.Sub = new System.Windows.Forms.Button();
+            this.Eql = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Num1
+            // 
+            this.Num1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num1.Location = new System.Drawing.Point(21, 108);
+            this.Num1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(46, 50);
+            this.Num1.TabIndex = 0;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            this.Num1.Click += new System.EventHandler(this.Num1_Click);
+            // 
+            // Num2
+            // 
+            this.Num2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num2.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num2.Location = new System.Drawing.Point(72, 108);
+            this.Num2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(46, 50);
+            this.Num2.TabIndex = 1;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            this.Num2.Click += new System.EventHandler(this.Num2_Click);
+            // 
+            // Num3
+            // 
+            this.Num3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num3.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num3.Location = new System.Drawing.Point(123, 108);
+            this.Num3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(46, 50);
+            this.Num3.TabIndex = 2;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            this.Num3.Click += new System.EventHandler(this.Num3_Click);
+            // 
+            // Num4
+            // 
+            this.Num4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num4.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num4.Location = new System.Drawing.Point(21, 163);
+            this.Num4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(46, 50);
+            this.Num4.TabIndex = 3;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            this.Num4.Click += new System.EventHandler(this.Num4_Click);
+            // 
+            // Num5
+            // 
+            this.Num5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num5.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num5.Location = new System.Drawing.Point(72, 163);
+            this.Num5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(46, 50);
+            this.Num5.TabIndex = 4;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            this.Num5.Click += new System.EventHandler(this.Num5_Click);
+            // 
+            // Num6
+            // 
+            this.Num6.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num6.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num6.Location = new System.Drawing.Point(123, 163);
+            this.Num6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(46, 50);
+            this.Num6.TabIndex = 5;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            this.Num6.Click += new System.EventHandler(this.Num6_Click);
+            // 
+            // Num7
+            // 
+            this.Num7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num7.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num7.Location = new System.Drawing.Point(21, 219);
+            this.Num7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(46, 50);
+            this.Num7.TabIndex = 6;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            this.Num7.Click += new System.EventHandler(this.Num7_Click);
+            // 
+            // Num8
+            // 
+            this.Num8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num8.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num8.Location = new System.Drawing.Point(72, 219);
+            this.Num8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(46, 50);
+            this.Num8.TabIndex = 7;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            this.Num8.Click += new System.EventHandler(this.Num8_Click);
+            // 
+            // Num9
+            // 
+            this.Num9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num9.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num9.Location = new System.Drawing.Point(123, 219);
+            this.Num9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(46, 50);
+            this.Num9.TabIndex = 8;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            this.Num9.Click += new System.EventHandler(this.Num9_Click);
+            // 
+            // Num0
+            // 
+            this.Num0.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Num0.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Num0.Location = new System.Drawing.Point(72, 274);
+            this.Num0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(46, 50);
+            this.Num0.TabIndex = 9;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            this.Num0.Click += new System.EventHandler(this.Num0_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Clear.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.Clear.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Clear.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Clear.Location = new System.Drawing.Point(21, 274);
+            this.Clear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(46, 50);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "C";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Add.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.Add.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Add.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Add.Font = new System.Drawing.Font("Calibri Light", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Add.Location = new System.Drawing.Point(174, 108);
+            this.Add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(46, 50);
+            this.Add.TabIndex = 11;
+            this.Add.Text = "+";
+            this.Add.UseVisualStyleBackColor = false;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Sub
+            // 
+            this.Sub.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Sub.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.Sub.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Sub.Font = new System.Drawing.Font("Calibri Light", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sub.Location = new System.Drawing.Point(174, 163);
+            this.Sub.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Sub.Name = "Sub";
+            this.Sub.Size = new System.Drawing.Size(46, 50);
+            this.Sub.TabIndex = 12;
+            this.Sub.Text = "-";
+            this.Sub.UseVisualStyleBackColor = false;
+            this.Sub.Click += new System.EventHandler(this.Sub_Click);
+            // 
+            // Eql
+            // 
+            this.Eql.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Eql.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.Eql.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Eql.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Eql.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Eql.Location = new System.Drawing.Point(174, 274);
+            this.Eql.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Eql.Name = "Eql";
+            this.Eql.Size = new System.Drawing.Size(46, 50);
+            this.Eql.TabIndex = 13;
+            this.Eql.Text = "=";
+            this.Eql.UseVisualStyleBackColor = false;
+            this.Eql.Click += new System.EventHandler(this.Eql_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBox.Location = new System.Drawing.Point(21, 19);
+            this.TextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.ReadOnly = true;
+            this.TextBox.Size = new System.Drawing.Size(200, 51);
+            this.TextBox.TabIndex = 14;
+            this.TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Label
+            // 
+            this.Label.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label.Location = new System.Drawing.Point(22, 69);
+            this.Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(198, 25);
+            this.Label.TabIndex = 15;
+            this.Label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 346);
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.Eql);
+            this.Controls.Add(this.Sub);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(260, 385);
+            this.MinimumSize = new System.Drawing.Size(260, 385);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Sub;
+        private System.Windows.Forms.Button Eql;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Label Label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Проект2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int Numb1, Numb2, count;
+        private void Num1_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 1;
+        }
+
+        private void Num2_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 2;
+        }
+
+        private void Num3_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 3;
+        }
+
+        private void Num4_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 4;
+        }
+
+        private void Num5_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 5;
+        }
+
+        private void Num6_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 6;
+        }
+
+        private void Num7_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 7;
+        }
+
+        private void Num8_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 8;
+        }
+
+        private void Num9_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 9;
+        }
+
+        private void Num0_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += 0;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Numb1 = int.Parse(TextBox.Text);
+                TextBox.Clear();
+                count = 1;
+                Label.Text = Numb1.ToString() + "+";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вы не ввели число!");
+            }
+        }
+
+        private void Sub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Numb1 = int.Parse(TextBox.Text);
+                TextBox.Clear();
+                count = 2;
+                Label.Text = Numb1.ToString() + "-";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вы не ввели число!");
+            }
+        }
+
+        private void Eql_Click(object sender, EventArgs e)
+        {
+            Calculate();
+            Label.Text = "";
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Label.Text = "";
+            TextBox.Text = "";
+        }
+
+        public void Calculate()
+        {
+            try
+            {
+                switch (count)
+                {
+                    case 1:
+                        Numb2 = Numb1 + int.Parse(TextBox.Text);
+                        TextBox.Text = Numb2.ToString();
+                        break;
+                    case 2:
+                        Numb2 = Numb1 - int.Parse(TextBox.Text);
+                        TextBox.Text = Numb2.ToString();
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Вы не ввели второе число!");
+            }
+        }
+    }
+}
